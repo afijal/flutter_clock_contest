6d47eb3bfa326a08045773bf3b139f36eff94c2e
Artur Fijał <arturfijal@Arturs-Mac-mini.local>
2020-01-21 03:36:37 +0100

    Added licence

--- digital_analog_clock/lib/hand_postions.dart
+++ digital_analog_clock/lib/hand_postions.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 class HandPositions {
   //actually hour and minutes are of same length so it won't matter
   final double hour;
